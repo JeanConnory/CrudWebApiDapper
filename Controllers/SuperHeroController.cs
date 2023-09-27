399482c5a75d5ff142c75945b79c1439e46ad4ce
jean.connory <jean.connory@outlook.com>
2023-09-26 21:51:57 -0300

    add dapper, sqlclient and getall method

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using CrudWebApiDapper.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CrudWebApiDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("Select * from superheroes");
+            return Ok(heroes);
+        }
+    }
+}
